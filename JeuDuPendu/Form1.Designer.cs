8e3248c9f05ecf4264a61a2267d5876ef22125d3
marin <marin@DESKTOP-EFT9TBC>
2023-09-19 10:18:01 +0200

    Ajoutez des fichiers projet.

---
+++ JeuDuPendu/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace JeuDuPendu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtMotAChercher = new System.Windows.Forms.TextBox();
+            this.cmbLettres = new System.Windows.Forms.ComboBox();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.lblLettres = new System.Windows.Forms.Label();
+            this.pict = new System.Windows.Forms.PictureBox();
+            this.btnRejouer = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pict)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtMotAChercher
+            // 
+            this.txtMotAChercher.Location = new System.Drawing.Point(18, 19);
+            this.txtMotAChercher.Name = "txtMotAChercher";
+            this.txtMotAChercher.Size = new System.Drawing.Size(121, 20);
+            this.txtMotAChercher.TabIndex = 0;
+            // 
+            // cmbLettres
+            // 
+            this.cmbLettres.FormattingEnabled = true;
+            this.cmbLettres.Location = new System.Drawing.Point(15, 19);
+            this.cmbLettres.Name = "cmbLettres";
+            this.cmbLettres.Size = new System.Drawing.Size(40, 21);
+            this.cmbLettres.TabIndex = 1;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(71, 19);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(52, 21);
+            this.btnTest.TabIndex = 2;
+            this.btnTest.Text = "TEST";
+            this.btnTest.UseVisualStyleBackColor = true;
+            // 
+            // lblLettres
+            // 
+            this.lblLettres.AutoSize = true;
+            this.lblLettres.Enabled = false;
+            this.lblLettres.Location = new System.Drawing.Point(12, 55);
+            this.lblLettres.Name = "lblLettres";
+            this.lblLettres.Size = new System.Drawing.Size(35, 13);
+            this.lblLettres.TabIndex = 3;
+            this.lblLettres.Text = "label1";
+            // 
+            // pict
+            // 
+            this.pict.Enabled = false;
+            this.pict.Location = new System.Drawing.Point(195, 12);
+            this.pict.Name = "pict";
+            this.pict.Size = new System.Drawing.Size(159, 173);
+            this.pict.TabIndex = 4;
+            this.pict.TabStop = false;
+            // 
+            // btnRejouer
+            // 
+            this.btnRejouer.Image = ((System.Drawing.Image)(resources.GetObject("btnRejouer.Image")));
+            this.btnRejouer.Location = new System.Drawing.Point(150, 148);
+            this.btnRejouer.Name = "btnRejouer";
+            this.btnRejouer.Size = new System.Drawing.Size(39, 37);
+            this.btnRejouer.TabIndex = 5;
+            this.btnRejouer.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Enabled = false;
+            this.label1.Location = new System.Drawing.Point(32, 172);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtMotAChercher);
+            this.groupBox1.Location = new System.Drawing.Point(20, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(169, 50);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Mot à chercher";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cmbLettres);
+            this.groupBox2.Controls.Add(this.btnTest);
+            this.groupBox2.Controls.Add(this.lblLettres);
+            this.groupBox2.Location = new System.Drawing.Point(20, 68);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(169, 77);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Lettres testées";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 203);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRejouer);
+            this.Controls.Add(this.pict);
+            this.Name = "Form1";
+            this.Text = "Jeu du pendu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pict)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMotAChercher;
+        private System.Windows.Forms.ComboBox cmbLettres;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Label lblLettres;
+        private System.Windows.Forms.PictureBox pict;
+        private System.Windows.Forms.Button btnRejouer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ JeuDuPendu/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JeuDuPendu
+{
+    public partial class Form1 : Form
+    {
+        public void rempliCombo()
+        {
+            //vide le combo
+            cmbLettres.Items.Clear();
+
+            //rempli le combo avec lettre alphabet
+            for (int i=0; i<26; i++)
+            {
+                cmbLettres.Items.Add((char)('A'+i));
+            }
+            cmbLettres.SelectedIndex = 0;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rempliCombo();
+        }
+    }
+}
