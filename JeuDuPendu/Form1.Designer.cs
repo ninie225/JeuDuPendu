5a3ef7011ebe35316d5972f8e69ebabfa57b7bf4
marin <marine.runavot@hotmail>
2023-10-06 18:39:42 +0200

    Sprint 2 et 3 : Gérer la navigation et sécurisation de l'interface. Créer fonctionnalités

--- JeuDuPendu/Form1.Designer.cs
+++ JeuDuPendu/Form1.Designer.cs
@@ -33,84 +33,96 @@
             this.cmbLettres = new System.Windows.Forms.ComboBox();
             this.btnTest = new System.Windows.Forms.Button();
             this.lblLettres = new System.Windows.Forms.Label();
-            this.pict = new System.Windows.Forms.PictureBox();
+            this.imgPendu = new System.Windows.Forms.PictureBox();
             this.btnRejouer = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblReussite = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pict)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgPendu)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtMotAChercher
             // 
-            this.txtMotAChercher.Location = new System.Drawing.Point(18, 19);
+            this.txtMotAChercher.Location = new System.Drawing.Point(24, 23);
+            this.txtMotAChercher.Margin = new System.Windows.Forms.Padding(4);
             this.txtMotAChercher.Name = "txtMotAChercher";
-            this.txtMotAChercher.Size = new System.Drawing.Size(121, 20);
+            this.txtMotAChercher.Size = new System.Drawing.Size(160, 22);
             this.txtMotAChercher.TabIndex = 0;
+            this.txtMotAChercher.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMotAChercher_KeyPress);
             // 
             // cmbLettres
             // 
             this.cmbLettres.FormattingEnabled = true;
-            this.cmbLettres.Location = new System.Drawing.Point(15, 19);
+            this.cmbLettres.Location = new System.Drawing.Point(20, 23);
+            this.cmbLettres.Margin = new System.Windows.Forms.Padding(4);
             this.cmbLettres.Name = "cmbLettres";
-            this.cmbLettres.Size = new System.Drawing.Size(40, 21);
+            this.cmbLettres.Size = new System.Drawing.Size(52, 24);
             this.cmbLettres.TabIndex = 1;
             // 
             // btnTest
             // 
-            this.btnTest.Location = new System.Drawing.Point(71, 19);
+            this.btnTest.Location = new System.Drawing.Point(95, 23);
+            this.btnTest.Margin = new System.Windows.Forms.Padding(4);
             this.btnTest.Name = "btnTest";
-            this.btnTest.Size = new System.Drawing.Size(52, 21);
+            this.btnTest.Size = new System.Drawing.Size(69, 26);
             this.btnTest.TabIndex = 2;
             this.btnTest.Text = "TEST";
             this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
             // 
             // lblLettres
             // 
             this.lblLettres.AutoSize = true;
             this.lblLettres.Enabled = false;
-            this.lblLettres.Location = new System.Drawing.Point(12, 55);
+            this.lblLettres.Location = new System.Drawing.Point(16, 68);
+            this.lblLettres.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblLettres.Name = "lblLettres";
-            this.lblLettres.Size = new System.Drawing.Size(35, 13);
+            this.lblLettres.Size = new System.Drawing.Size(0, 17);
             this.lblLettres.TabIndex = 3;
-            this.lblLettres.Text = "label1";
             // 
-            // pict
+            // imgPendu
             // 
-            this.pict.Enabled = false;
-            this.pict.Location = new System.Drawing.Point(195, 12);
-            this.pict.Name = "pict";
-            this.pict.Size = new System.Drawing.Size(159, 173);
-            this.pict.TabIndex = 4;
-            this.pict.TabStop = false;
+            this.imgPendu.Enabled = false;
+            this.imgPendu.Location = new System.Drawing.Point(260, 15);
+            this.imgPendu.Margin = new System.Windows.Forms.Padding(4);
+            this.imgPendu.Name = "imgPendu";
+            this.imgPendu.Size = new System.Drawing.Size(212, 213);
+            this.imgPendu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imgPendu.TabIndex = 4;
+            this.imgPendu.TabStop = false;
             // 
             // btnRejouer
             // 
+            this.btnRejouer.Cursor = System.Windows.Forms.Cursors.Default;
             this.btnRejouer.Image = ((System.Drawing.Image)(resources.GetObject("btnRejouer.Image")));
-            this.btnRejouer.Location = new System.Drawing.Point(150, 148);
+            this.btnRejouer.Location = new System.Drawing.Point(200, 182);
+            this.btnRejouer.Margin = new System.Windows.Forms.Padding(4);
             this.btnRejouer.Name = "btnRejouer";
-            this.btnRejouer.Size = new System.Drawing.Size(39, 37);
+            this.btnRejouer.Size = new System.Drawing.Size(52, 46);
             this.btnRejouer.TabIndex = 5;
             this.btnRejouer.UseVisualStyleBackColor = true;
+            this.btnRejouer.Click += new System.EventHandler(this.btnRejouer_Click);
             // 
-            // label1
+            // lblReussite
             // 
-            this.label1.AutoSize = true;
-            this.label1.Enabled = false;
-            this.label1.Location = new System.Drawing.Point(32, 172);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "label1";
+            this.lblReussite.AutoSize = true;
+            this.lblReussite.Enabled = false;
+            this.lblReussite.Location = new System.Drawing.Point(43, 212);
+            this.lblReussite.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblReussite.Name = "lblReussite";
+            this.lblReussite.Size = new System.Drawing.Size(0, 17);
+            this.lblReussite.TabIndex = 6;
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.txtMotAChercher);
-            this.groupBox1.Location = new System.Drawing.Point(20, 12);
+            this.groupBox1.Location = new System.Drawing.Point(27, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(169, 50);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(225, 62);
             this.groupBox1.TabIndex = 7;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Mot à chercher";
@@ -120,27 +132,30 @@
             this.groupBox2.Controls.Add(this.cmbLettres);
             this.groupBox2.Controls.Add(this.btnTest);
             this.groupBox2.Controls.Add(this.lblLettres);
-            this.groupBox2.Location = new System.Drawing.Point(20, 68);
+            this.groupBox2.Location = new System.Drawing.Point(27, 84);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(169, 77);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(225, 95);
             this.groupBox2.TabIndex = 8;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Lettres testées";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(372, 203);
+            this.ClientSize = new System.Drawing.Size(496, 250);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblReussite);
             this.Controls.Add(this.btnRejouer);
-            this.Controls.Add(this.pict);
+            this.Controls.Add(this.imgPendu);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Jeu du pendu";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pict)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgPendu)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
@@ -156,9 +171,9 @@
         private System.Windows.Forms.ComboBox cmbLettres;
         private System.Windows.Forms.Button btnTest;
         private System.Windows.Forms.Label lblLettres;
-        private System.Windows.Forms.PictureBox pict;
+        private System.Windows.Forms.PictureBox imgPendu;
         private System.Windows.Forms.Button btnRejouer;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblReussite;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
     }
--- JeuDuPendu/Form1.cs
+++ JeuDuPendu/Form1.cs
@@ -12,6 +12,23 @@ namespace JeuDuPendu
 {
     public partial class Form1 : Form
     {
+        /// <summary>
+        /// mot à rechercher
+        /// </summary>
+        private string mot;
+
+        /// <summary>
+        /// étape d'affichage du pendu
+        /// </summary>
+        private int etapePendu;
+
+        /// <summary>
+        /// maximum d'étapes du pendu
+        /// </summary>
+        private int maxPendu = 10;
+        /// <summary>
+        /// Vide le combo et le rempli avec les lettres de l'alphabet
+        /// </summary>
         public void rempliCombo()
         {
             //vide le combo
@@ -24,14 +41,225 @@ namespace JeuDuPendu
             }
             cmbLettres.SelectedIndex = 0;
         }
+
+        /// <summary>
+        /// Initialisation des objets graphiques
+        /// </summary>
         public Form1()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Préparation de la phase 1
+        /// Saisie du mot à chercher
+        /// </summary>
+        public void phase1()
+        {
+            //réinitialise l'étape du pendu et affiche l'image vide
+            etapePendu = 0;
+            AfficheImage(etapePendu);
+
+            //désactive la zone de la phase 2 (proposition de lettres) et le bouton TEST
+            cmbLettres.Enabled = false;
+            btnTest.Enabled = false;
+
+            //supprime le message et l'affichage des lettres testées
+            lblReussite.Text = "";
+            lblLettres.Text = "";
+
+            //active, vide et se positionne sur la zone de saisie du mot
+            txtMotAChercher.Enabled = true;
+            txtMotAChercher.Text = "";
+            txtMotAChercher.Focus();
+        }
+
+        /// <summary>
+        /// Préparation de la phase 2 
+        /// Recherche du mot
+        /// </summary>
+        public void phase2()
+        {
+            //Rend impossible l'accès à la case de saisie du mot à chercher
+            txtMotAChercher.Enabled = false;
+            //active la zone de la phase 2 (proposition de lettres)
+            cmbLettres.Enabled = true;
+            cmbLettres.SelectedIndex = 0;
+            cmbLettres.Focus();
+
+            btnTest.Enabled = true;
+        }
+
+
+        private void AfficheImage(int num)
+        {
+            imgPendu.ImageLocation = Application.StartupPath + "/../../Properties/ImagesJeuPendu/pendu" + num + ".png";
+            imgPendu.Image = (Image)Properties.Resources.ResourceManager.GetObject("pendu" + num);
+        }
+        /// <summary>
+        /// Contrôle si un mot est bien constitué uniquement de lettres
+        /// </summary>
+        /// <param name="unMot"></param>
+        /// <returns></returns>
+        public bool MotCorrect(string unMot)
+        {
+            bool correct = true;
+            if (unMot!="" && unMot.Length<16)
+            {
+                unMot = unMot.ToUpper();
+                for (int k = 0; k < unMot.Length; k++)
+                {
+                    if (unMot[k] < 'A' || unMot[k] > 'Z')
+                    {
+                        correct = false;
+                    }
+                }
+            }
+            else
+            {
+                correct = false;
+            }
+            return correct;
+        }
+        /// <summary>
+        /// Evenement chargement de la fenêtre
+        /// Création des boutons des lettres de l'alphabet
+        /// et démarrage du jeu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Form1_Load(object sender, EventArgs e)
         {
+            phase1();
             rempliCombo();
+        }
+
+        /// <summary>
+        /// Evenement touche enfoncée dans la zone du mot
+        /// si validation, enregistrement du mot et début du jeu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtMotAChercher_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //validation donc fin de la saisie du mot
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                //vérifie qu'un mot a été tapé et qu'il est correct (uniquement des lettres et taille inférieure à 16)
+                if (MotCorrect(txtMotAChercher.Text))
+                {
+                    //met le mot en majuscule et le mémorise dans une propriété
+                    mot = txtMotAChercher.Text.ToUpper();
+                    //remplit la zone de tirets à la place des lettres
+                    txtMotAChercher.Text = "";
+                    for (int i=0; i<mot.Length; i++)
+                    {
+                        txtMotAChercher.Text += "-";
+                    }
+                    //Préparation des objets graphiques pour la phase 2 (recherche du mot)
+                    phase2();
+                }
+                else
+                {
+                    MessageBox.Show("Le mot ne doit comporter que des lettres alphabétiques (pas d'espaces, pas d'accent)");
+                    phase1();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Evenement clic sur le bouton TEST
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            if (cmbLettres.SelectedIndex!= -1)
+            {
+                char lettre = char.Parse(cmbLettres.SelectedItem.ToString());
+                lblLettres.Text += lettre;
+                if (!RechercheLettreDansMot(lettre))
+                {
+                    //lettre non trouvée : affichage du pendu
+                    if (AffichePendu())
+                    {
+                        //si totalement pendu, perdu et fin du jeu
+                        FinJeu("PERDU");
+                    }
+                    else
+                    {
+                        //il n'y a plus de lettre à trouver
+                        if (txtMotAChercher.Text.IndexOf("-")== -1)
+                        {
+                            //s'il n'y a plus de tiret (toutes les lettres sont trouvées) c'est gagné
+                            FinJeu("GAGNE");
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Affichage d'une étape du pendu
+        /// </summary>
+        /// <returns></returns>
+        private bool AffichePendu()
+        {
+            etapePendu++;
+            AfficheImage(etapePendu);
+            return (etapePendu == maxPendu);
+        }
+
+
+        /// <summary>
+        /// Recherche la lettre dans le mot et remplace le tiret par la lettre
+        /// Retourne vrai si la lettre est trouvée au moins une fois
+        /// </summary>
+        /// <param name="lettre"> lettre à chercher </param>
+        /// <returns></returns>
+        private bool RechercheLettreDansMot(char lettre)
+        {
+            //position de la lettre dans le mot
+            int position = -1;
+            //booléen qui va dire si la lettre a été trouvée ou non
+            bool trouve = false;
+
+            //boucle sur la recherche de la lettre (qui peut être présente plusieurs fois)
+            do
+            {
+                //récupère la position de la lettre (ou -1)
+                position = mot.IndexOf(lettre, position + 1);
+                if (position != -1)
+                {
+                    trouve = true;
+                    //remplace le tiret par la lettre dans la zone de texte
+                    txtMotAChercher.Text = txtMotAChercher.Text.Remove(position, 1);
+                    txtMotAChercher.Text = txtMotAChercher.Text.Insert(position, lettre.ToString());
+                }
+            } while (position != -1);
+            return trouve;
+
+        }
+
+        private void FinJeu(string message)
+        {
+            //affichage du message (gagné ou perdu)
+            lblReussite.Text = message;
+            //affiche le mot correct
+            txtMotAChercher.Text = mot;
+            //désactive la zone de proposition de lettre
+            cmbLettres.Enabled = false;
+            //se positionne sur le bouton recommencer (btnRejouer)
+            btnRejouer.Focus();
+        }
+        /// <summary>
+        /// Evenement clic sur le bouton rejouer
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnRejouer_Click(object sender, EventArgs e)
+        {
+            Form1_Load(null, null);
         }
     }
 }
